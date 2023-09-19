2f80871fe5d88bd700c941862c3c1c97e2ee3fc7
Noah DeCambra <70386429+noahdecambra@users.noreply.github.com>
2023-09-19 18:25:03 -0400

    Timber Final Version
    - Finished my own style of Timber
    - Player must defend themself from enemies on left and right side of the screen
    - Use left and right arrow keys to change direction
    - Use spacebar to attack the enemies
    - Careful not to get touched or game over
    - Increasing difficulty with each round (capped at lvl 5)
    - Beautiful ambient rain & space ships flying overhead
    - README in timber folder includes more details referring to project requirements

--- GameProg1/Timber/README.txt
+++ GameProg1/Timber/README.txt
@@ -1,1 +1,29 @@
-Hello
+Prescribed Change #1
+- New graphics
+	- Player
+	- Enemy
+	- Space ships x3
+	- Background
+	- Rain
+	- Light saber (axe)
+- New sound effects
+	- Light saber hit
+	- Game over sound
+- New font
+	- Star jedi
+
+Prescribed Change #2
+- Flexible video mode
+- Background fills entire screen
+
+Four choices
+- A.
+	- Enemies move horizontally coming from both sides of the screen
+- C.
+	- Rain falls vertically down the screen
+- D.
+	- Space ships randomly change size when entering screen
+- E.
+	- Game gets more difficult with each level (difficulty stops increasing at level 5)
+- F.
+	- Player needs to attack with space bar
--- GameProg1/Timber/Timber_DeCambra.cpp
+++ GameProg1/Timber/Timber_DeCambra.cpp
@@ -1,28 +1,47 @@
 // Include important C++ libraries here
 #include <sstream>
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
 // Make code easier to type with "using namespace"
 using namespace sf;
 
 // Function declaration
-void updateBranches(int seed);
+void updateEnemies(float elapsedTime, int enemyPositions[], bool enemyAlive[], float& enemySpeed, int& numAliveEnemies);
 
-const int NUM_BRANCHES = 6;
-Sprite branches[NUM_BRANCHES];
+static int spawnedEnemies = 0;
+const int NUM_ENEMIES = 7;
+float enemySpeed = 235.0f;
+Sprite enemies[NUM_ENEMIES];
 
-// Where is player/branhc?
-// Left or right
+// Increasing difficulty
+int score = 0;
+int currentLevel = 1;
+float enemySpawnInterval = 1.0f;
+
+// Rain
+const int MAX_RAIN_DROPS = 500;
+Sprite rain[MAX_RAIN_DROPS];
+
+// Where is player?
 enum class side {LEFT, RIGHT, NONE};
-side branchPositions[NUM_BRANCHES];
 
 int main()
 {
 	// Create a video mode object
-	VideoMode vm(1920, 1080);
+	// VideoMode vm(1920, 1080);
+
+	// Low res code
+	VideoMode vm(960, 540);
 
 	// Create and open a window for the game
-	RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+	// RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+
+	// Low res code
+	RenderWindow window(vm, "Timber!!!");
+	View view(sf::FloatRect(0, 0, 1920, 1080));
+	window.setView(view);
+	//End of low res code!! Everything else is the same!!
 
 	// Create a texture to hold a graphic on the GPU
 	Texture textureBackground;
@@ -39,25 +58,19 @@ int main()
 	// Set the spriteBackground to cover the screen
 	spriteBackground.setPosition(0, 0);
 
-	// Make a tree sprite
-	Texture textureTree;
-	textureTree.loadFromFile("graphics/tree.png");
-	Sprite spriteTree;
-	spriteTree.setTexture(textureTree);
-	spriteTree.setPosition(810, 0);
+	// Prepare the rain
+	Texture textureRain;
+	textureRain.loadFromFile("graphics/rain.png");
+	float rainDelay = 0.0f;
+	static int nextRaindropIndex = 0;
 
-	// Prepare the bee
-	Texture textureBee;
-	textureBee.loadFromFile("graphics/bee.png");
-	Sprite spriteBee;
-	spriteBee.setTexture(textureBee);
-	spriteBee.setPosition(0, 800);
+	for (int i = 0; i < MAX_RAIN_DROPS; i++)
+	{
+		rain[i].setTexture(textureRain);
+	}
 
-	// Is the bee currently moving?
-	bool beeActive = false;
-
-	// How fast can the bee fly?
-	float beeSpeed = 25.0f;
+	// How fast does the rain fall?
+	float rainSpeed = 400.0f;
 
 	// Make 3 ships sprites
 	Texture textureShip1;
@@ -104,26 +117,13 @@ int main()
 	// Variables to control time itself
 	Clock clock;
 
-	// Time bar
-	RectangleShape timeBar;
-	float timeBarStartWidth = 400;
-	float timeBarHeight = 80;
-	timeBar.setSize(Vector2f(timeBarStartWidth, timeBarHeight));
-	timeBar.setFillColor(Color::Red);
-	timeBar.setPosition((1920 / 2) - timeBarStartWidth / 2, 980);
-
-	Time gameTimeTotal;
-	float timeRemaining = 6.0f;
-	float timeBarWidthPerSecond = timeBarStartWidth / timeRemaining;
-
 	// Track whether the game is running
-	bool paused = true;
+	bool paused = true;	
 
 	// Draw some text
-	int score = 0;
-
 	sf::Text messageText;
 	sf::Text scoreText;
+	sf::Text levelText;
 
 	// We need to choose a font
 	sf::Font font;
@@ -132,56 +132,110 @@ int main()
 	// Set the font to our message
 	messageText.setFont(font);
 	scoreText.setFont(font);
+	levelText.setFont(font);
 
 	// Assign the actual message
 	messageText.setString("Press Enter to start!");
 	scoreText.setString("Score: 0");
+	levelText.setString("LvL: " + std::to_string(currentLevel));
 
 	// Make it really big
 	messageText.setCharacterSize(75);
 	scoreText.setCharacterSize(100);
+	levelText.setCharacterSize(100);
 
 	// Choose a color
-	messageText.setFillColor(Color::White);
-	scoreText.setFillColor(Color::White);
+	messageText.setFillColor(Color::Green);
+	scoreText.setFillColor(Color::Green);
+	levelText.setFillColor(Color::Green);
 
 	// Position the text
 	FloatRect textRect = messageText.getLocalBounds();
-
-	messageText.setOrigin(textRect.left +
-		textRect.width / 2.0f,
-		textRect.top +
-		textRect.height / 2.0f);
-
+	messageText.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
 	messageText.setPosition(1920 / 2.0f, 1080 / 2.0f);
+	scoreText.setPosition(70, 20);
+	levelText.setPosition(1400, 20);
 
-	scoreText.setPosition(20, 20);
+	// Prepare the enemy
+	Texture textureEnemy;
+	textureEnemy.loadFromFile("graphics/enemy.png");
+	int enemyPositions[NUM_ENEMIES];
+	bool enemyAlive[NUM_ENEMIES];
+	int numAliveEnemies = 0;
 
-	// Prepare 5 branches
-	Texture textureBranch;
-	textureBranch.loadFromFile("graphics/branch.png");
-
-	// Set the texture for each branch sprite
-	for (int i = 0; i < NUM_BRANCHES; i++)
+	for (int i = 0; i < NUM_ENEMIES; i++)
 	{
-		branches[i].setTexture(textureBranch);
-		branches[i].setPosition(-2000, -2000);
-
-		// Set the sprite's origin to dead center
-		// We can then spin it round without changing its position
-		branches[i].setOrigin(220, 20);
+		enemies[i].setTexture(textureEnemy);
+		enemies[i].setPosition(-2000, -2000);
+		enemyAlive[i] = false;
 	}
 
-	updateBranches(1);
-	updateBranches(2);
-	updateBranches(3);
-	updateBranches(4);
-	updateBranches(5);
+	// Prepare the player
+	Texture	texturePlayer;
+	texturePlayer.loadFromFile("graphics/player.png");
+	Sprite spritePlayer;
+	spritePlayer.setTexture(texturePlayer);
+	spritePlayer.setPosition(960, 764);
+	spritePlayer.setOrigin(38.5, 0);
+
+	// The player starts on the left
+	side playerSide = side::LEFT;
+
+	// Prepare the gravestone
+	Texture	textureRIP;
+	textureRIP.loadFromFile("graphics/rip.png");
+	Sprite spriteRIP;
+	spriteRIP.setTexture(textureRIP);
+	spriteRIP.setPosition(600, 830);
+
+	// Prepare the axe
+	Texture	textureAxe;
+	textureAxe.loadFromFile("graphics/axe.png");
+	Sprite spriteAxe;
+	spriteAxe.setTexture(textureAxe);
+	spriteAxe.setPosition(940, 890);
+	spriteAxe.setRotation(45);
+	spriteAxe.setScale(-1, 1);
+
+	// Line the axe up with the player
+	const float AXE_POSITION_LEFT = 940;
+	const float AXE_POSITION_RIGHT = 960;
+	bool axePositionLeft = false;
+	bool axePositionRight = false;
+
+	// Control the player input
+	bool acceptInput = false;
+
+	// Prepare the sound
+	SoundBuffer lightsaberBuffer;
+	lightsaberBuffer.loadFromFile("sound/lightsaber.wav");
+	Sound lightsaber;
+	lightsaber.setBuffer(lightsaberBuffer);
+
+	SoundBuffer deathBuffer;
+	deathBuffer.loadFromFile("sound/death.wav");
+	Sound death;
+	death.setBuffer(deathBuffer);
 
 	while (window.isOpen())
 	{
-		// Score ++;
-
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::KeyReleased && !paused)
+			{
+				if (axePositionLeft)
+				{
+					spriteAxe.setRotation(45);
+				}
+				else
+				{
+					spriteAxe.setRotation(-45);
+				}
+				// Listen for key presses again
+				acceptInput = true;
+			}
+		}
 		/*
 		****************************************
 		Handle the players input
@@ -192,15 +246,170 @@ int main()
 		{
 			window.close();
 		}
-
+	
 		// Start the game
 		if (Keyboard::isKeyPressed(Keyboard::Return))
 		{
 			paused = false;
-
-			// Reset the time and score
+	
+			// Reset the score and level and enemy speed
 			score = 0;
-			timeRemaining = 5;
+			currentLevel = 1;
+			levelText.setString("LvL: " + std::to_string(currentLevel));
+			enemySpawnInterval = 1.0f;
+			enemySpeed = 235.0f;
+	
+			// Hide all enemies
+			for (int i = 0; i < NUM_ENEMIES; i++)
+			{
+				enemyAlive[i] = false;
+				numAliveEnemies = 0;
+			}
+
+			// Reset the rain
+			nextRaindropIndex = 0;
+			rainDelay = 0.0f;
+			for (int i = 0; i < MAX_RAIN_DROPS; i++)
+			{
+				rain[i].setPosition(-2000, -2000);
+			}
+	
+			// Make sure the gravestone is hidden
+			spriteRIP.setPosition(675, 2000);
+		
+			// Set axe position
+			spriteAxe.setPosition(AXE_POSITION_LEFT, spriteAxe.getPosition().y);
+			spriteAxe.setScale(-1, 1);
+			axePositionRight = false;
+			axePositionLeft = true;
+			if (axePositionLeft)
+			{
+				spriteAxe.setRotation(45);
+			}
+			else
+			{
+				spriteAxe.setRotation(-45);
+			}
+				
+			// Set player position
+			spritePlayer.setOrigin(38.5, 0);
+			spritePlayer.setPosition(960, 764);
+			spritePlayer.setScale(1, 1);
+			acceptInput = true;
+		}
+	
+		// Wrap the player control to make sure we are accepting input
+		if (acceptInput)
+		{
+			// Attack if space key is pressed
+			if (Keyboard::isKeyPressed(Keyboard::Space))
+			{
+				// Check if player hit enemy
+				for (int i = 0; i < NUM_ENEMIES; i++)
+				{
+					float enemyX = enemies[i].getPosition().x;
+					float playerX = spritePlayer.getPosition().x;
+					float enemyHitDistanceX = 125.0f;
+
+					float distanceFromPlayerX = std::abs(enemyX - playerX);
+
+					if (enemyAlive[i] && distanceFromPlayerX <= enemyHitDistanceX)
+					{
+						if ((playerSide == side::LEFT && enemyPositions[i] < 5) || (playerSide == side::RIGHT && enemyPositions[i] > 5))
+						{
+							score++;
+							enemyAlive[i] = false;
+							numAliveEnemies--;							
+						}
+					}
+				}
+
+				// Axe swing
+				if (axePositionLeft)
+				{
+					spriteAxe.setRotation(-45);
+				}
+				else
+				{
+					spriteAxe.setRotation(45);
+				}
+				acceptInput = false;
+
+				// Play a lightsaber sound
+				lightsaber.play();
+
+				// Update levels based on score
+				if (score >= currentLevel * 10)
+				{
+					currentLevel++;
+					levelText.setString("LvL: " + std::to_string(currentLevel));
+
+					enemySpeed += 100.0f;
+					if (enemySpeed >= 700.0f)
+					{
+						// Cap enemy speed
+						enemySpeed = 700.0f;
+					}
+
+					enemySpawnInterval -= 0.2f;
+					if (enemySpawnInterval <= 0.4f)
+					{
+						// Cap enemy spawn interval
+						enemySpawnInterval = 0.4f;
+					}
+				}
+			}
+
+
+			// First handle pressing the right cursor key
+			if (Keyboard::isKeyPressed(Keyboard::Right))
+			{
+				// Axe and player positioning
+				axePositionLeft = false;
+				axePositionRight = true;
+				if (axePositionLeft)
+				{
+					spriteAxe.setRotation(45);
+				}
+				else
+				{
+					spriteAxe.setRotation(-45);
+				}
+				spriteAxe.setPosition(AXE_POSITION_RIGHT, spriteAxe.getPosition().y);
+				spriteAxe.setScale(1, 1);
+				
+				playerSide = side::RIGHT;
+				
+				spritePlayer.setPosition(960, 764);
+				spritePlayer.setScale(-1, 1);
+				
+				acceptInput = false;
+			}
+
+			// Handle the left cursor key
+			if (Keyboard::isKeyPressed(Keyboard::Left))
+			{
+				// Axe and player positioning
+				axePositionLeft = true;
+				axePositionRight = false;
+				if (axePositionLeft)
+				{
+					spriteAxe.setRotation(45);
+				}
+				else
+				{
+					spriteAxe.setRotation(-45);
+				}
+				spriteAxe.setPosition(AXE_POSITION_LEFT, spriteAxe.getPosition().y);
+				spriteAxe.setScale(-1, 1);
+				
+				playerSide = side::LEFT;
+
+				spritePlayer.setPosition(960, 764);
+				spritePlayer.setScale(1, 1);
+
+				acceptInput = false;
+			}
 		}
 
 		/*
@@ -213,58 +422,36 @@ int main()
 		{
 			// Measure time
 			Time dt = clock.restart();
+			
+			// Increment rainDelay
+			rainDelay += dt.asSeconds();
 
-			// Subtract from the amount of time remaining
-			timeRemaining -= dt.asSeconds();
-
-			// size up the time bar
-			timeBar.setSize(Vector2f(timeBarWidthPerSecond *
-				timeRemaining, timeBarHeight));
-
-
-			if (timeRemaining <= 0.0f)
+			// Set the rain
+			if (rainDelay >= 0.5f)
 			{
-
-				// Pause the game
-				paused = true;
-
-				// Change the message shown to the player
-				messageText.setString("out of time!!");
-
-				//Reposition the text based on its new size
-				FloatRect textRect = messageText.getLocalBounds();
-				messageText.setOrigin(textRect.left +
-					textRect.width / 2.0f,
-					textRect.top +
-					textRect.height / 2.0f);
-
-				messageText.setPosition(1920 / 2.0f, 1080 / 2.0f);
-			}
-
-
-			// Set the bee
-			if (!beeActive)
-			{
-				// How fast is the bee
-				srand((int)time(0));
-				beeSpeed = (rand() % 200) + 200;
-
-				// How high is the bee
-				srand((int)time(0) * 10);
-				float height = (rand() % 500) + 500;
-				spriteBee.setPosition(2000, height);
-				beeActive = true;
-			}
-			else
-				// Move the bee
-			{
-				spriteBee.setPosition(spriteBee.getPosition().x - (beeSpeed * dt.asSeconds()), spriteBee.getPosition().y);
-
-				// Has the bee reached the left-hand edge of the screen?
-				if (spriteBee.getPosition().x < -100)
+				if (nextRaindropIndex < MAX_RAIN_DROPS)
 				{
-					// Set it up ready to be a whole new bee next frame
-					beeActive = false;
+					// Where does the rain start
+					float xAxis = static_cast<float>(rand() % 1920);
+					float yAxis = 0.0f;
+					rain[nextRaindropIndex].setPosition(xAxis, yAxis);
+
+					nextRaindropIndex++;
+					rainDelay = 0.0f;
+				}
+			}
+
+			// Move all the rain drops
+			for (int i = 0; i < nextRaindropIndex; i++)
+			{
+				rain[i].move(0, rainSpeed * dt.asSeconds());
+
+				// Has rain reached the ground?
+				if (rain[i].getPosition().y >= 930)
+				{
+					// Reset raindrop's position to the top
+					float xAxis = static_cast<float>(rand() % 1920);
+					rain[i].setPosition(xAxis, 0);
 				}
 			}
 
@@ -290,7 +477,6 @@ int main()
 			}
 			else
 			{
-
 				spriteShip1.setPosition(spriteShip1.getPosition().x + (ship1Speed * dt.asSeconds()), spriteShip1.getPosition().y);
 
 				// Has the ship reached the right hand edge of the screen?
@@ -304,7 +490,6 @@ int main()
 			// ship 2
 			if (!ship2Active)
 			{
-
 				// How fast is the ship
 				srand((int)time(0) * 20);
 				ship2Speed = (rand() % 200);
@@ -323,7 +508,6 @@ int main()
 			}
 			else
 			{
-
 				spriteShip2.setPosition(spriteShip2.getPosition().x + (ship2Speed * dt.asSeconds()), spriteShip2.getPosition().y);
 
 				// Has the ship reached the right hand edge of the screen?
@@ -337,7 +521,6 @@ int main()
 			// ship 3
 			if (!ship3Active)
 			{
-
 				// How fast is the ship
 				srand((int)time(0) * 30);
 				ship3Speed = (rand() % 200);
@@ -356,7 +539,6 @@ int main()
 			}
 			else
 			{
-
 				spriteShip3.setPosition(spriteShip3.getPosition().x + (ship3Speed * dt.asSeconds()), spriteShip3.getPosition().y);
 
 				// Has the ship reached the right hand edge of the screen?
@@ -372,32 +554,45 @@ int main()
 			ss << "Score: " << score;
 			scoreText.setString(ss.str());
 
-			// Update the branch sprites
-			for (int i = 0; i < NUM_BRANCHES; i++)
+			// Update the enemy positions
+			updateEnemies(dt.asSeconds(), enemyPositions, enemyAlive, enemySpeed, numAliveEnemies);
+	
+			// Has the player been killed by enemy?
+			for (int i = 0; i < NUM_ENEMIES; i++)
 			{
-				float height = i * 150;
+				float playerHitDistanceX = 1.0f; // Adjust this distance as needed
+				float playerX = spritePlayer.getPosition().x;
+				float enemyX = enemies[i].getPosition().x;
+
+				float distanceFromPlayerX = std::abs(enemyX - playerX);
+
+				if (enemyAlive[i] && distanceFromPlayerX < playerHitDistanceX)
+				{
+					// death
+					paused = true;
+					acceptInput = false;
+
+					// Draw the gravestone
+					spriteRIP.setPosition(900, 850);
+
+					// hide the player & axe
+					spritePlayer.setPosition(2000, 660);
+					spriteAxe.setPosition(3000, spriteAxe.getPosition().y);
+
+					// Change the text of the message
+					messageText.setString("game over!");
+
+					// Center it on the screen
+					FloatRect textRect = messageText.getLocalBounds();
+
+					messageText.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
+
+					messageText.setPosition(1920 / 2.0f, 1080 / 2.0f);
 				
-				if (branchPositions[i] == side::LEFT)
-				{
-					// Move the sprite to the left side
-					branches[i].setPosition(600, height);
-					// Flip the sprite around the other way
-					branches[i].setRotation(180);
-				}
-				else if (branchPositions[i] == side::RIGHT)
-				{
-					// Move the sprite to the right side
-					branches[i].setPosition(1330, height);
-					// Flip the sprite rotation to normal
-					branches[i].setRotation(0);
-				}
-				else
-				{
-					// Hide the branch
-					branches[i].setPosition(3000, height);
+					// Play the death sound
+					death.play();
 				}
 			}
-
 		}// End if(!pause)
 
 		/*
@@ -417,23 +612,38 @@ int main()
 		window.draw(spriteShip2);
 		window.draw(spriteShip3);
 
-		// Draw the branches
-		for (int i = 0; i < NUM_BRANCHES; i++)
+		// Draw the enemies
+		for (int i = 0; i < NUM_ENEMIES; i++)
 		{
-			window.draw(branches[i]);
+			if (enemyAlive[i])
+			{
+				window.draw(enemies[i]);
+			}
 		}
-
-		// Draw the tree
-		window.draw(spriteTree);
-
-		// Draw the insect
-		window.draw(spriteBee);
-
+	
+		// Draw the player
+		window.draw(spritePlayer);
+	
+		// Draw the axe
+		window.draw(spriteAxe);
+	
+		// Draw the RIP
+		window.draw(spriteRIP);
+	
+		// Draw the rain
+		if (!paused)
+		{
+			for (int i = 0; i < MAX_RAIN_DROPS; i++)
+			{
+				window.draw(rain[i]);
+			}
+		}		
+	
 		// Draw the score
 		window.draw(scoreText);
 
-		// Draw the timebar
-		window.draw(timeBar);
+		// Draw the level text
+		window.draw(levelText);
 
 		if (paused)
 		{
@@ -447,31 +657,73 @@ int main()
 	return 0;
 }
 
-// Function definition
-void updateBranches(int seed)
+void updateEnemies(float elapsedTime, int enemyPositions[], bool enemyAlive[], float& enemySpeed, int& numAliveEnemies)
 {
-	// Move all the branches down one place
-	for (int j = NUM_BRANCHES - 1; j > 0; j--)
-	{
-		branchPositions[j] = branchPositions[j - 1];
-	}
+    static float timeSinceLastMove = 0.0f;
+    timeSinceLastMove += elapsedTime;
 
-	// Spawn a new branch at position 0
-	// LEFT, RIGHT, or NONE
-	srand((int)time(0) + seed);
-	int r = (rand() % 5);
+    if (timeSinceLastMove >= enemySpawnInterval && numAliveEnemies < NUM_ENEMIES)
+    {
+        // Find an inactive enemy slot and spawn a new enemy
+        for (int i = 0; i < NUM_ENEMIES; i++)
+        {
+            if (!enemyAlive[i])
+            {
+                // Spawn a new enemy on a random side
+                int randomSide = rand() % 2;
+                float height = 850;
 
-	switch (r)
-	{
-		case 0:
-			branchPositions[0] = side::LEFT;
-				break;
+                if (randomSide == 0)
+                {
+					// Left side
+                    enemyPositions[i] = 0;
+                    enemies[i].setPosition(92.5, height);
+                    enemies[i].setScale(-1, 1);
+                }
+                else
+                {
+					// Right side
+                    enemyPositions[i] = 10;
+                    enemies[i].setPosition(1827.5, height);
+                    enemies[i].setScale(1, 1);
+                }
 
-		case 1:
-			branchPositions[0] = side::RIGHT;
+                enemyAlive[i] = true;
+                numAliveEnemies++;
+                break; // Exit the loop after spawning an enemy
+            }
+        }
 
-		default:
-			branchPositions[0] = side::NONE;
-			break;
-	}
+        timeSinceLastMove = 0.0f;
+    }
+
+    // Update the positions of all alive enemies
+    for (int j = 0; j < NUM_ENEMIES; j++)
+    {
+        if (enemyAlive[j])
+        {
+            // Move the enemy based on its position
+            if (enemyPositions[j] < 5)
+            {
+				// Left to right
+                enemies[j].move(enemySpeed * elapsedTime, 0);
+                enemies[j].setOrigin(0, 0);
+            }
+            else if (enemyPositions[j] > 5)
+            {
+				// Right to left
+                enemies[j].move(-enemySpeed * elapsedTime, 0);
+                enemies[j].setOrigin(0, 0);
+            }
+
+            // Check if an enemy has moved off-screen
+            if (enemies[j].getPosition().x < -200 || enemies[j].getPosition().x > 2120)
+            {
+                // Reset the enemy's position and mark it as inactive
+                enemies[j].setPosition(-2000, -2000);
+                enemyAlive[j] = false;
+                numAliveEnemies--;
+            }
+        }
+    }
 }
